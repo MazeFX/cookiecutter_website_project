f4e1ea82fec5b600ff16bdb970da0739abc996b5
MazeFX <filoplast@gmail.com>
2016-07-12 11:38:08 +0200

    Built first own unit test for pages app

--- website/pages/tests.py
+++ website/pages/tests.py
@@ -1,3 +1,19 @@
+# -*- coding: utf-8 -*-
+
+# GIFS - Clean up the file
+# DOCS - Write some documentation
+
+"""
+File: tests.py
+Creator: MazeFX
+Date: 12-7-2016
+
+Tests written for test website pages (home, about, contact, etc)
+"""
+
 from django.test import TestCase
 
-# Create your tests here.
+class SmokeTest(TestCase):
+
+    def test_bad_maths(self):
+        self.assertEqual(1 + 1, 3)
