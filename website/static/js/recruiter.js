7b15670e7949a487e89613b662f5d5255e046af0
MazeFX <filoplast@gmail.com>
2016-07-15 22:47:24 +0200

    Added parallax javascript

--- website/static/js/recruiter.js
+++ website/static/js/recruiter.js
@@ -6,9 +6,10 @@
  * JS for recruiter page.
  * Startup functions for js.
  */
-/* jQuery(document).ready(function(){
+
+jQuery(document).ready(function(){
     jQuery('#parallax .parallax-layer')
     .parallax({
       mouseport: jQuery('#parallax')
     });
-}); */
+});
