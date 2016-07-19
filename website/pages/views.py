acb0ab3b2839f2229684d387a2c3f6838e805ded
MazeFX <filoplast@gmail.com>
2016-07-19 21:48:32 +0200

    testing for email

--- website/pages/views.py
+++ website/pages/views.py
@@ -18,5 +18,5 @@ def home_page(request):
 def send_email(request):
     send_mail("It works!", "This will get sent through Mailgun",
     "Anymail Mailgun says BOOM!!! <MazeFX@MazeFX.pythonanywhere.com>", ["filoplast@gmail.com"])
-    return render(request, 'pages/home.html')
+    return render(request, 'pages/about.html')
 
