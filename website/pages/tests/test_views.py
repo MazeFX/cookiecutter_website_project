5d425b13bf9857c467368e5ed7debe2930f38680
MazeFX <filoplast@gmail.com>
2016-07-27 12:49:26 +0200

    Unit test for sending email passed.

--- website/pages/tests/test_views.py
+++ website/pages/tests/test_views.py
@@ -11,10 +11,12 @@ Contact page has the ability to send emails through anymail/mailgun.
 """
 
 from django.core.urlresolvers import resolve
+from django.core import mail
 from django.test import TestCase
 from django.http import HttpRequest
 from django.template.loader import render_to_string
 from django.utils.html import escape
+
 
 from website.pages.views import home_page, contact_page
 from website.pages.forms import ContactForm, EMPTY_ITEM_ERROR
@@ -56,15 +58,6 @@ class ContactTest(TestCase):
 
         self.assertIsInstance(response.context['form'], ContactForm)
 
-    def test_contact_page_returns_a_POST_request_on_submit(self):
-        request = HttpRequest()
-        request.method = 'POST'
-        request.POST['fullname'] = 'Dave'
-
-        response = contact_page(request)
-
-        self.assertIn('Dave', response.content.decode())
-
     def test_for_invalid_input_renders_contact_template(self):
         response = self.client.post('/contact/', data={'fullname': ''})
 
@@ -80,5 +73,28 @@ class ContactTest(TestCase):
         response = self.client.post('/contact/', data={'fullname': ''})
 
         self.assertIsInstance(response.context['form'], ContactForm)
+
+    def test_contact_page_returns_a_POST_request_on_submit(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['fullname'] = 'Dave'
+
+        response = contact_page(request)
+
+        self.assertIn('Dave', response.content.decode())
+
+    def test_contact_page_sends_an_email_on_submit(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['fullname'] = 'Dave'
+        request.POST['email'] = 'from@MazeFXtester.com'
+        request.POST['subject'] = 'Unit test mail'
+        request.POST['message'] = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum et purus ' \
+                                  'interdum dui auctor tempus eget vel sapien. Etiam in.'
+
+        response = contact_page(request)
+
+        self.assertEqual(len(mail.outbox), 1)
+        self.assertEqual(mail.outbox[0].subject, 'New contact form submission from recruiter mail.')
 
 
--- website/pages/views.py
+++ website/pages/views.py
@@ -9,7 +9,8 @@ Views written for rendering main website pages (home, about, contact, etc).
 """
 
 from django.shortcuts import redirect, render
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
+from django.template.loader import get_template
 
 from website.pages.forms import ContactForm
 
@@ -23,6 +24,28 @@ def contact_page(request):
     if request.method == 'POST':
         form = ContactForm(data=request.POST)
         if form.is_valid():
-            print('-----------------------Form is valid___________')
+            fullname = request.POST.get('fullname', '')
+            email = request.POST.get('email', '')
+            subject = request.POST.get('subject', '')
+            message = request.POST.get('message', '')
+
+            template = get_template('pages/contact_email.txt')
+            context = {
+                'fullname': fullname,
+                'email': email,
+                'subject': subject,
+                'message': message,
+            }
+            content = template.render(context)
+
+            email = EmailMessage(
+                "New contact form submission from recruiter mail.",
+                content,
+                "MazeFXwebsite@contactform.com",
+                ['filoplast@gmail.com'],
+                headers={'Reply-To': email}
+            )
+            email.send()
             return redirect('../contact/mail_sent/')
+        return render(request, 'pages/contact.html', {"form": form})
     return render(request, 'pages/contact.html', {"form": form})
