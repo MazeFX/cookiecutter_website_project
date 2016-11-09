1f5db8d55634977721f0f48c0d5882ae289a0811
MazeFX <filoplast@gmail.com>
2016-11-09 22:45:44 +0100

    created nav toggler open menu on hover.

--- website/static/js/recruiter.js
+++ website/static/js/recruiter.js
@@ -18,5 +18,24 @@ jQuery(document).ready(function(){
         jQuery('.parallax-layer')
         .trigger({ type: 'freeze', x: 0.5, y: 0.5, decay: 0 });
     }
+
+    jQuery('.navbar-toggler').hover(
+        function() {
+            console.log('Hovering into menu..');
+            jQuery('.navbar-toggler').trigger('click');
+        }, function() {
+            console.log('Hovering out of menu..');
+            jQuery('.navbar-toggler').trigger('click');
+        }
+    );
+
+    jQuery('#exCollapsingNavbar').hover(
+        function() {
+            console.log('Hovering into inner nav..');
+        }, function() {
+            console.log('Hovering out of inner nav..');
+            jQuery('.navbar-toggler').trigger('click');
+        }
+    );
 });
 
